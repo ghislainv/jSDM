3583b3b2f6a89858236327b9b40a4f3e1bc5869f
JeanneClement <47427109+JeanneClement@users.noreply.github.com>
2019-05-24 09:19:26 +0200

    Delete "gsl_linalg.h" which serve only to gsl_chol_decomp and add "mvgauss.c"

--- src/Rcpp_jSDM_useful.cpp
+++ src/Rcpp_jSDM_useful.cpp
@@ -9,10 +9,12 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_matrix.h>
-#include <gsl/gsl_linalg.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_cdf.h>
 
+extern "C" {
+#include "mvgauss.c"
+}
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::depends(RcppGSL)]]
 
